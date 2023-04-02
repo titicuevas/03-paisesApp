e6754dcfa043884252ac1a2c89addeea70f89754
titicuevas <enrique.cuevas@iesdonana.org>
2023-04-02 19:29:40 +0200

    Captura de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,22 +5,25 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
-    
+
   ]
 })
 export class PorPaisComponent {
 
   termino : string = '';
-
+  hayError: boolean = false;
     constructor ( private PaisService: PaisService){}
 
       buscar(){
 
+        this.hayError = false;
         console.log(this.termino);
 
 
-        this.PaisService.buscarPais( this.termino).subscribe(resp=>{
+        this.PaisService.buscarPais( this.termino).subscribe((resp)=>{
           console.log(resp);
+        }, (err)=>{
+         this.hayError = true;
         });
 
     }
